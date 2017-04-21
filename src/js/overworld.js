0085d08ced5acbb35a2994cfff6c0c88cbf3a6a7
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2017-04-21 10:42:07 -0400

    Make pedestrians smarter about following goals.
    There are a ton of improvements to be made here. Right now we're
    BFS-ing each time a pedestrian has a new goal, but we could be
    A*-ing or even preprocessing the map. If preprocessing, look into
    'minimum dissection into rectangles of a rectilinear polygon'.

--- src/js/overworld.js
+++ src/js/overworld.js
@@ -9,18 +9,20 @@ window.myGame = window.myGame || {};
         this.map.setCollisionByExclusion([], true, 'building');
         this.buildingLayer.resizeWorld();
 
-        this.floorTiles = this.getFloorTiles();
+        this.loadFloorTiles();
     };
 
-    Overworld.prototype.getFloorTiles = function() {
-        var safeTiles = [];
+    Overworld.prototype.loadFloorTiles = function() {
+        this.floorTiles = [];
+        this.pointMap = {};
+        var self = this;
         var tiles = this.floorLayer.getTiles(0, 0, this.map.widthInPixels, this.map.heightInPixels);
         tiles.forEach(function(tile) {
-            if (tile.index == 1) {
-                safeTiles.push(tile);
+            if (tile.index === 1) {
+                self.floorTiles.push(tile);
+                self.pointMap[tile.x + " " + tile.y] = true
             }
-        })
-        return safeTiles;
+        });
     };
 
     Overworld.prototype.constructor = Overworld;
@@ -29,7 +31,63 @@ window.myGame = window.myGame || {};
         // Get a random point on the floor (not in a building)
         var tile = Phaser.ArrayUtils.getRandomItem(this.floorTiles);
         return new Phaser.Point(tile.worldX, tile.worldY);
-    }
+    };
+
+    Overworld.prototype.buildPath = function(parents, targetNode) {
+        var result = [targetNode];
+        var key = targetNode.x + " " + targetNode.y;
+        while (parents[key] !== null) {
+            targetNode = parents[key];
+            result.push(targetNode);
+            key = targetNode.x + " " + targetNode.y;
+        }
+        return result.reverse();
+    };
+
+    Overworld.prototype.findPath = function (start, end) {
+        start = {x: Math.floor(start.x / 256), y: Math.floor(start.y / 256)}
+        end = {x: Math.floor(end.x / 256), y: Math.floor(end.y / 256)}
+        var queue = [start];
+        var parents = {};
+        parents[start.x + " " + start.y] = null;
+        var visited = {};
+        visited[start.x + " " + start.y] = true;
+        while (queue.length) {
+            var current = queue.shift();
+            if (current.x === end.x && current.y === end.y) {
+                var path = this.buildPath(parents, end);
+                path = path.map(function(step) {
+                    return {x: step.x * 256 + 128, y: step.y * 256 + 128};
+                });
+                return path;
+            }
+            this.floorNeighbors(current).forEach(function(pt) {
+                var key = pt.x + " " + pt.y;
+                if (!visited[key]) {
+                    parents[key] = current;
+                    visited[key] = true;
+                    queue.push(pt);
+                }
+            });
+        }
+    };
+
+    Overworld.prototype.floorNeighbors = function (point) {
+        var potential = [
+            {x: point.x + 1, y: point.y},
+            {x: point.x - 1, y: point.y},
+            {x: point.x, y: point.y + 1},
+            {x: point.x, y: point.y - 1},
+        ];
+        var neighbors = [];
+        var self = this;
+        potential.forEach(function(pt) {
+            if (self.pointMap[pt.x + " " + pt.y]) {
+                neighbors.push(pt);
+            }
+        });
+        return neighbors;
+    };
 
     myGame.Overworld = Overworld;
 })(window.Phaser, window.myGame);
--- src/js/pedestrian.js
+++ src/js/pedestrian.js
@@ -1,12 +1,11 @@
 window.myGame = window.myGame || {};
 
 (function(Phaser, myGame) {
-    const ACCEPTABLE_GOAL_DISTANCE = 20;
+    const ACCEPTABLE_GOAL_DISTANCE = 200;
     const Pedestrian = function(game, overworld) {
         this.overworld = overworld;
         var start = overworld.randomFloorPoint();
         myGame.Actor.call(this, game, start.x, start.y, 'pedestrian');
-        this.goal = new Phaser.Point();
         this.setNewGoal();
     }
     Pedestrian.prototype = Object.create(myGame.Actor.prototype);
@@ -14,17 +13,25 @@ window.myGame = window.myGame || {};
 
     Pedestrian.prototype.update = function() {
         var speed = Pedestrian.prototype.MOVE_SPEED;
-        var distance2 = Phaser.Math.distanceSq(this.goal.x, this.goal.y, this.body.x, this.body.y);
+        var next = this.path[this.pathIndex]
+        var distance2 = Phaser.Math.distanceSq(next.x, next.y, this.body.x, this.body.y);
         if (distance2 < ACCEPTABLE_GOAL_DISTANCE * ACCEPTABLE_GOAL_DISTANCE) {
-            this.setNewGoal();
+            this.pathIndex++;
+            if (this.pathIndex >= this.path.length) {
+                this.setNewGoal();
+            }
+            next = this.path[this.pathIndex]
         }
-        this.body.acceleration.x = Phaser.Math.clamp(this.goal.x - this.body.x, -speed, speed);
-        this.body.acceleration.y = Phaser.Math.clamp(this.goal.y - this.body.y, -speed, speed);
+        this.body.acceleration.x = Phaser.Math.clamp(next.x - this.body.x, -speed, speed);
+        this.body.acceleration.y = Phaser.Math.clamp(next.y - this.body.y, -speed, speed);
         myGame.Actor.prototype.update.call(this);
     };
 
     Pedestrian.prototype.setNewGoal = function() {
-        this.goal = this.overworld.randomFloorPoint();
+        var goal = this.overworld.randomFloorPoint();
+        this.path = this.overworld.findPath(this.body, goal);
+        this.pathIndex = 0;
     };
+
     myGame.Pedestrian = Pedestrian;
 })(window.Phaser, window.myGame);
