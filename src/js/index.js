2853a12262b802bb0b67cfbeaeef8b050327c879
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2017-04-20 23:00:38 -0400

    Add overworld to navigate.
    Alright, alright, I gotta admit Tiled is pretty sweet.
    I should start encapsulating this stuff sooner rather than later.

--- src/js/index.js
+++ src/js/index.js
@@ -1,64 +1,78 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-var player;
+window.myGame = window.myGame || {};
 
-const MOVE_SPEED = 1500;
+(function(Phaser, myGame) {
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    var player;
+    var map;
+    var buildingLayer;
 
-function preload() {
-    game.load.image('pedestrian', 'assets/sprites/pedestrian.png');
-    game.load.image('background', 'assets/tilemaps/bg.png');
-}
+    const MOVE_SPEED = 1500;
 
-function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.add.sprite(0, 0, 'background');
-
-    player = game.add.sprite(384, game.world.height - 150, 'pedestrian');
-    game.physics.arcade.enable(player);
-    player.body.drag.x = 1000;
-    player.body.drag.y = 1000;
-
-    cursors = game.input.keyboard.createCursorKeys();
-}
-
-function constrainVelocity(sprite, maxVelocity) {
-    var body = sprite.body;
-    var angle, currVelocitySqr, vx, vy;
-    vx = body.velocity.x;
-    vy = body.velocity.y;
-    currVelocitySqr = vx * vx + vy * vy;
-    if (currVelocitySqr > maxVelocity * maxVelocity) {
-        angle = Math.atan2(vy, vx);
-        vx = Math.cos(angle) * maxVelocity;
-        vy = Math.sin(angle) * maxVelocity;
-        body.velocity.x = vx;
-        body.velocity.y = vy;
-        console.log('limited speed to: '+maxVelocity);
+    function preload() {
+        game.load.image('pedestrian', 'assets/sprites/pedestrian.png');
+        game.load.image('background', 'assets/tilemaps/bg.png');
+        game.load.tilemap('level', 'assets/levels/level0.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('tiles', 'assets/tilemaps/overworld.png');
     }
-};
 
+    function create() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.add.sprite(0, 0, 'background');
 
-function update() {
-    // player.body.velocity.x = 0;
-    // player.body.velocity.y = 0;
-    // console.log(player.body.velocity);
-    constrainVelocity(player, 300);
-    if (cursors.left.isDown) {
-        player.body.acceleration.x = -MOVE_SPEED;
-    }
-    else if (cursors.right.isDown) {
-        player.body.acceleration.x = MOVE_SPEED;
-    }
-    else {
-        player.body.acceleration.x = 0;
+        // map shit
+        map = game.add.tilemap('level', 256, 256);
+        map.addTilesetImage('Crowd', 'tiles');
+        map.createLayer('floor');
+        buildingLayer = map.createLayer('building');
+        map.setCollisionByExclusion([], true, 'building');
+        //  Resize the world
+        buildingLayer.resizeWorld();
 
+        player = game.add.sprite(384, 384, 'pedestrian');
+        game.physics.arcade.enable(player);
+        player.body.drag.x = 1000;
+        player.body.drag.y = 1000;
+
+        cursors = game.input.keyboard.createCursorKeys();
+        game.camera.follow(player);
     }
-    if (cursors.up.isDown) {
-        player.body.acceleration.y = -MOVE_SPEED;
+
+    function constrainVelocity(sprite, maxVelocity) {
+        var body = sprite.body;
+        var angle, currVelocitySqr, vx, vy;
+        vx = body.velocity.x;
+        vy = body.velocity.y;
+        currVelocitySqr = vx * vx + vy * vy;
+        if (currVelocitySqr > maxVelocity * maxVelocity) {
+            angle = Math.atan2(vy, vx);
+            vx = Math.cos(angle) * maxVelocity;
+            vy = Math.sin(angle) * maxVelocity;
+            body.velocity.x = vx;
+            body.velocity.y = vy;
+        }
     }
-    else if (cursors.down.isDown) {
-        player.body.acceleration.y = MOVE_SPEED;
+
+    function update() {
+        constrainVelocity(player, 300);
+        if (cursors.left.isDown) {
+            player.body.acceleration.x = -MOVE_SPEED;
+        }
+        else if (cursors.right.isDown) {
+            player.body.acceleration.x = MOVE_SPEED;
+        }
+        else {
+            player.body.acceleration.x = 0;
+
+        }
+        if (cursors.up.isDown) {
+            player.body.acceleration.y = -MOVE_SPEED;
+        }
+        else if (cursors.down.isDown) {
+            player.body.acceleration.y = MOVE_SPEED;
+        }
+        else {
+            player.body.acceleration.y = 0;
+        }
+        game.physics.arcade.collide(player, buildingLayer);
     }
-    else {
-        player.body.acceleration.y = 0;
-    }
-}
+})(window.Phaser, window.myGame);
